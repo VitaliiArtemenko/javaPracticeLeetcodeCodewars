35261053efd9acc7c52c32982268a65c1bef19e4
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-12 23:03:27 -0700

    Sum of positive - 8 kuy

---
+++ src/codewars/kuy8/sum/of/positive/SumOfPositive.java
@@ -0,0 +1,9 @@
+package sum.of.positive;
+
+import java.util.Arrays;
+
+public class SumOfPositive {
+    static int sumOfPositive(int[] arr) {
+        return Arrays.stream(arr).filter(el -> el > 0).sum();
+    }
+}
