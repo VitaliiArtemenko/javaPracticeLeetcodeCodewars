6ec5def4e0cd3db0365b23b18665973e01c20b12
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-12 22:16:37 -0700

    Even or Odd

---
+++ src/codewars/kuy8/even/or/odd/EvenOrOdd.java
@@ -0,0 +1,12 @@
+package even.or.odd;
+
+public class EvenOrOdd {
+
+    public static String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
