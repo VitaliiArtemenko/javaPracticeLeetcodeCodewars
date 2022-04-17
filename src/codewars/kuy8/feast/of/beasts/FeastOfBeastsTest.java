00d331bd4a79e582653184202cc7ab32171d8aab
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-17 13:08:23 -0700

    The Feast of Many Beasts - 8 kuy

---
+++ src/codewars/kuy8/feast/of/beasts/FeastOfBeastsTest.java
@@ -0,0 +1,16 @@
+package feast.of.beasts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FeastOfBeastsTest {
+
+    @Test
+    void feastOfBeastsTest() {
+        assertTrue(FeastOfBeasts.feastOfBeasts("great blue heron","garlic nann"));
+        assertTrue(FeastOfBeasts.feastOfBeasts("chickadee","chocolate cake"));
+        assertFalse(FeastOfBeasts.feastOfBeasts("brown bear","bear claw"));
+        assertFalse(FeastOfBeasts.feastOfBeasts("Great blue heron","garlic nann"));
+    }
+}
