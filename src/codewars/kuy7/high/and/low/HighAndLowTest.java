ef561beaf9d270cddfa246e6bbae4685db162209
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-18 20:16:10 -0700

    Highest and Lowest - 7 kuy

---
+++ src/codewars/kuy7/high/and/low/HighAndLowTest.java
@@ -0,0 +1,16 @@
+package high.and.low;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HighAndLowTest {
+
+    @Test
+    void highAndLowTest() {
+        assertEquals("42 -9", HighAndLow.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        assertEquals("3 1", HighAndLow.highAndLow("1 2 3"));
+        assertEquals("0 -1", HighAndLow.highAndLow("-1 -1 0"));
+        assertEquals("617 -971", HighAndLow.highAndLow("364 -856 -971 350 -487 -484 617 -139 607"));
+    }
+}
