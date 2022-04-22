f5640c1cf74232db82096cee6a01a0dcd82b4242
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-04-21 21:31:28 -0700

    Sort My Textbooks - 8 kuy

---
+++ src/codewars/kuy8/sort/my/textbooks/SortMyTextbooks.java
@@ -0,0 +1,11 @@
+package sort.my.textbooks;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortMyTextbooks {
+    static List<String> sortMyTextbooks(List<String> textbooks) {
+        textbooks.sort(String.CASE_INSENSITIVE_ORDER);
+        return textbooks;
+    }
+}
