f97cc71db8866dd15721d7b2b5de4ff300f4d42c
VArtemenko <vitaliiart1977@gmail.com>
2022-05-06 10:11:38 -0700

    Jumping Number (Special Numbers Series #4) - 6 kuy

---
+++ src/codewars/kuy7/jumping/number/JumpingNumberTest.java
@@ -0,0 +1,31 @@
+package jumping.number;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JumpingNumberTest {
+
+    @Test
+    public void testSingleDigitNumber()
+    {
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(9));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(1));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(7));
+    }
+    @Test
+    public void testTwoDigitNumber()
+    {
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(23));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(32));
+        assertEquals("Not!!", JumpingNumber.jumpingNumber(79));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(98));
+    }
+    @Test
+    public void testLargeNumbers()
+    {
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(8987));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(4343456));
+        assertEquals("Jumping!!", JumpingNumber.jumpingNumber(98789876));
+    }
+}
