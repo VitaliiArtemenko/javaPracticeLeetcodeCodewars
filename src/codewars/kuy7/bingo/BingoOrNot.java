ab8934d80fab1b41e2d6ac58253f452f695354e0
VArtemenko <vitaliiart1977@gmail.com>
2022-05-06 16:39:40 -0700

    Bingo ( Or Not )- 7 kuy

--- src/codewars/kuy7/bingo/BingoOrNot.java
+++ src/codewars/kuy7/bingo/BingoOrNot.java
@@ -6,12 +6,11 @@ public class BingoOrNot {
 
     public static String bingo(int[] numberArray){
         int[] numbers = {2, 9, 14, 7, 15};
-        String answer = "WIN";
         for (int number : numbers) {
             if (Arrays.stream(numberArray).noneMatch(el -> el == number)) {
-               answer = "LOSE";
+               return  "LOSE";
             }
         }
-        return answer;
+        return "WIN";
     }
 }
