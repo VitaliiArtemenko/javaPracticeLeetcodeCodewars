76a3cfdedd4078bb2153866e9dcb900cd24f63e5
VArtemenko <vitaliiart1977@gmail.com>
2022-05-06 11:51:54 -0700

    No one likes carrying change - 6 kuy

--- src/codewars/kuy6/coin/free/CoinFree.java
+++ src/codewars/kuy6/coin/free/CoinFree.java
@@ -6,14 +6,11 @@ public class CoinFree {
 
     public static int coinFree(int amount, int[] coinAmounts) {
         int coins = 0;
-
         Arrays.sort(coinAmounts);
 
-        while (amount > 0) {
-            for (int i = coinAmounts.length - 1; i >= 0; i--) {
-                coins += amount / coinAmounts[i];
-                amount -= coinAmounts[i] * (amount / coinAmounts[i]);
-            }
+        for (int i = coinAmounts.length - 1; i >= 0; i--) {
+            coins += amount / coinAmounts[i];
+            amount -= coinAmounts[i] * (amount / coinAmounts[i]);
         }
         return coins;
     }
